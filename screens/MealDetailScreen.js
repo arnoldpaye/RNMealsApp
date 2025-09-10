b9dceebcc4333b4b645d5510cf2439ea23aa0d14
Arnold Paye <arnoldpayet@gmail.com>
2025-09-10 16:54:53 -0400

    feat: integrate favorite meal toggle functionality into MealDetailScreen using FavoritesContext

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,5 @@
-import { useLayoutEffect } from "react";
-import {
-  Image,
-  Text,
-  ScrollView,
-  StyleSheet,
-  View,
-} from "react-native";
+import { useContext, useLayoutEffect } from "react";
+import { Image, Text, ScrollView, StyleSheet, View } from "react-native";
 
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
@@ -13,14 +7,22 @@ import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
 
 import { MEALS } from "../data/dummy-data";
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
-  const { mealId } = route.params;
+  const favoriteMealsCtx = useContext(FavoritesContext);
 
+  const { mealId } = route.params;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log("Button was pressed!");
+  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      favoriteMealsCtx.removeFavorite(mealId);
+    } else {
+      favoriteMealsCtx.addFavorite(mealId);
+    }
   }
 
   useLayoutEffect(() => {
@@ -28,14 +30,14 @@ function MealDetailScreen({ route, navigation }) {
       headerRight: () => {
         return (
           <IconButton
-            icon={"star"}
+            icon={mealIsFavorite ? "star" : "star-outline"}
             color={"white"}
-            onPress={headerButtonPressHandler}
+            onPress={changeFavoriteStatusHandler}
           />
         );
       },
     });
-  }, [navigation, headerButtonPressHandler]);
+  }, [navigation, changeFavoriteStatusHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
